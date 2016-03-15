6733307c66dbc474c9628d417a459371c4599ff9
Romero Meireles <romero.mfm@gmail.com>
2016-03-15 17:41:59 -0300

    and NotificationFx bar

--- app/app.module.js
+++ app/app.module.js
@@ -1,2 +1,1 @@
-angular.module('ministore', ['ngRoute', 'ngResource', 'angular-loading-bar']);
-//angular.constant('context', "/");
+angular.module('ministore', ['ngRoute', 'ngResource', 'angular-loading-bar', 'notification.fx']);
--- app/components/product/productController.js
+++ app/components/product/productController.js
@@ -1,7 +1,7 @@
 angular.module('ministore')
 .controller('ProductController',
-  ['$scope', '$routeParams', 'ProductResource',
-  function($scope, $routeParams, ProductResource) {
+  ['$scope', '$routeParams', 'ProductResource', '$location', 'notificationFactory',
+  function($scope, $routeParams, ProductResource, $location, notificationFactory) {
 
   $scope.initProduct = function() {
     $scope.product = {};
@@ -10,18 +10,21 @@ angular.module('ministore')
   //list all
   $scope.list = function() {
     $scope.products = ProductResource.query();
+    notificationFactory.showSuccess("Listing products", function(){});
   }
 
   //find one
   $scope.show = function() {
     $scope.product = ProductResource.get({id: $routeParams.id});
+    notificationFactory.showSuccess("Showing product " + $routeParams.id, function(){});
   }
 
   //new
   $scope.save = function() {
     $scope.product.active = "true";
     ProductResource.save({product: $scope.product}, function() {
-      window.location.href = '';
+      notificationFactory.showSuccess("Product saved successfully", function(){});
+      $location.path('');
     });
   }
 
@@ -34,14 +37,16 @@ angular.module('ministore')
   $scope.update = function() {
     $scope.product.active = "true";
     $scope.product.$update({id: $scope.product.id}, function() {
-      window.location.href = '#/products/' + $scope.product.id;
+      notificationFactory.showSuccess("Product updated successfully", function(){});
+      $location.path('#/products/' + $scope.product.id);
     });
   }
 
   //delete
   $scope.delete = function(product) {
     ProductResource.delete({id: product.id}, function() {
-      window.location.href = '';
+      notificationFactory.showSuccess("Product removed successfully", function(){});
+      $location.path('');
     });
   }
 }]);
