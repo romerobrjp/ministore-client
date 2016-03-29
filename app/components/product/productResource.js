4d66233d2082560738f7510640ae7e28938f8936
Romero Meireles <romero.mfm@gmail.com>
2016-03-29 00:45:12 -0300

    Setting host app in heroku as API uri

--- app/components/product/productResource.js
+++ app/components/product/productResource.js
@@ -1,5 +1,5 @@
 angular.module('ministore').factory('ProductResource', function ProductResourceFactory($resource) {
-  return $resource('http://localhost:3000/api/v1/products/:id', {}, {
+  return $resource('http://ministore-rails-api.herokuapp.com/api/v1/products/:id', {}, {
     update: {
       method: 'PUT'
     },
